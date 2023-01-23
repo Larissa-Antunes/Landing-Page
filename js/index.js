1b2ea6224d035f0b97b23e247bcfbba7d8d93387
Larissa Antunes de Camargo <70041436+Larissa-Antunes@users.noreply.github.com>
2023-01-23 13:49:09 -0300

    Ajustando

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
-function validar(){
-    var email=form.email.value;
-    var telefone=form.telefone.value;
-
-    if(email == ""){
+function validarForm(){
+    if(document.getElementById("email").value === ""){
       alert("Preencha o campo Email.");
-      form.email.focus();
-      return
+    }
+	 if(document.getElementById("telefone").value === ""){
+      alert("Preencha o campo Telefone.");
+
+    }
+	 if(document.getElementById("obs").value === ""){
+      alert("Preencha o campo Observação.");
+
     }
 }
+/*------------------------------------------------*/
 
-/*------------------------------------------------*/*****************
+console.log("email")
+console.log("telefone")
+console.log("obs")
 
